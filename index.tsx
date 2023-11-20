e51fbc9192d627fb70f74b2ecceb1b01d6baf9f5
FARHAN ANSARI <farhanansari@MacBook-Air-LAP-028.local>
2023-11-20 23:02:45 +1100

    insatlled some pacakges

---
+++ index.tsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+if (process.env.IS_MOCK) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { worker } = require('./mocks/handlers')
+  worker.start({
+    onUnhandledRequest: 'bypass',
+  })
+}
+
+const rootElement = document.getElementById('app')
+
+if (rootElement) {
+  const root = createRoot(rootElement)
+  root.render(<>Hello</>)
+}
