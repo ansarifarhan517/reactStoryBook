578bb26ec9556d4ac930333213da9b0771e0eecc
Raj Singh <singh.42raj@gmail.com>
2023-11-27 16:44:37 +0530

    Webpack (#1)
    * webpack config
    
    * changes
    
    ---------
    
    Co-authored-by: RAJ SINGH <rajsingh@MacBook-Air-LAP-113.local>

---
+++ webpack.common.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.umd.js',
+        library: "ui-library",
+        libraryTarget: 'umd',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader'
+                }
+            }
+        ]
+    },
+    resolve: {
+        extension: ['*', '.js', '.jsx', '.ts', '.tsx'],
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ],
+    external: {
+        'react-dom': {
+            commonjs: 'react-dom',
+            root: 'ReactDOM'
+        },
+        'styled-components': "styled-components",
+        react: {
+            commonjs: 'react',
+            root: 'React'
+        }
+    }
+}
