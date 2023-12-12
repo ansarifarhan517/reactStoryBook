8a7b1fd1dcaf1a4806e7afd9e44c8e274ace8a97
FARHAN ANSARI <farhanansari@MacBook-Air-LAP-028.local>
2023-12-12 22:20:32 +1100

    box component implemented

--- src/utilities/mixins.ts
+++ src/utilities/mixins.ts
@@ -1,7 +1,7 @@
 import { css } from 'styled-components'
-import { TFlexProps, TMarginProps, TPaddingProps } from './types'
+import { IBgcolorProps, IBorderProps, ISpacingPropsTypes, IcolorProps, TFlexProps, TMarginProps, TPaddingProps } from './types'
 
-export const marginMixins = css<TMarginProps>`
+export const marginMixin = css<TMarginProps>`
   margin: ${({ m }) => m};
   margin-top: ${({ mt, my }) => mt ?? my};
   margin-bottom: ${({ mb, my }) => mb ?? my};
@@ -16,9 +16,73 @@ export const paddingMixin = css<TPaddingProps>`
   padding-right: ${({ pr, px }) => pr ?? px};
 `
 export const flexMixin = css<TFlexProps>`
-  flex-direction: ${({ flexDirection }) => flexDirection};
-  justify-content: ${({ justifyContent }) => justifyContent};
-  align-items: ${({ alignItems }) => alignItems};
-  flex-grow: ${({ flexGrow }) => flexGrow};
-  flex-shrink: ${({ flexShrink }) => flexShrink};
+  flex-direction: ${({ flexdirection }) => flexdirection};
+  justify-content: ${({ justifycontent }) => justifycontent};
+  align-items: ${({ alignitems }) => alignitems};
+  flex-grow: ${({ flexgrow }) => flexgrow};
+  flex-shrink: ${({ flexshrink }) => flexshrink};
+`
+export const borderMixin = css<IBorderProps>`
+  border: ${({ border }) => `${border || 0}px solid black`};
+  border-top: ${({ borderTop }) =>
+    borderTop !== undefined && `${borderTop}px solid black`};
+  border-bottom: ${({ borderBottom }) =>
+    borderBottom !== undefined && `${borderBottom}px solid black`};
+  border-left: ${({ borderLeft }) =>
+    borderLeft !== undefined && `${borderLeft}px solid black`};
+  border-right: ${({ borderRight }) =>
+    borderRight !== undefined && `${borderRight}px solid black`};
+  border-color: ${({ borderColor, theme }) => colorFinder(borderColor, theme)};
+  border-radius: ${({ borderRadius }) =>
+    typeof borderRadius === 'number' ? `${borderRadius}px` : borderRadius};
+`
+
+export const colorFinder = (color: string = '', theme: any) =>  color
+  ?.split('.')
+  ?.reduce(
+    (state: any, property: string) => state?.[property], theme?.colors,
+  )
+
+export const colorMixin = css<IcolorProps>`
+  color: ${({ color, theme }) => colorFinder(color, theme)};
+`
+
+export const bgColorMixin = css<IBgcolorProps>`
+  background-color: ${({ bgcolor, theme }) => colorFinder(bgcolor, theme)};
+`
+export const spacingMixin = css<ISpacingPropsTypes>`
+  ${({ horizontalSpacing }) => {
+    if (!horizontalSpacing) {
+      return
+    }
+
+    const spacing =
+      horizontalSpacing +
+      '' +
+      (typeof horizontalSpacing !== 'string' ? 'px' : '')
+    return `
+    & > *{
+      margin-right: ${spacing};
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  `
+  }}
+  ${({ verticalSpacing }) => {
+    if (!verticalSpacing) {
+      return
+    }
+
+    const spacing =
+      verticalSpacing + '' + (typeof verticalSpacing !== 'string' ? 'px' : '')
+    return `
+    & > *{
+      margin-bottom: ${spacing};
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  `
+  }}
 `
